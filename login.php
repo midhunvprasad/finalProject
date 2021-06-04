a735a670d16f89be8656683baf5c4d748549f0fe
amalsajeev14 <66429015+amalsajeev14@users.noreply.github.com>
2021-06-04 11:10:01 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,117 @@
+<?php require_once('header.php'); ?>
+
+<body id="home2" class="user-login blog">
+    <?php require_once('main-header.php'); ?>
+
+    <?php
+if(isset($_POST['form1'])) {
+        
+    if(empty($_POST['cust_email']) || empty($_POST['cust_password'])) {
+        $error_message = LANG_VALUE_132.'<br>';
+    } else {
+        
+        $cust_email = strip_tags($_POST['cust_email']);
+        $cust_password = strip_tags($_POST['cust_password']);
+
+        $statement = $pdo->prepare("SELECT * FROM tbl_customer C
+        left join tbl_country CO on C.cust_country=CO.country_id
+        WHERE C.cust_email=?");
+        $statement->execute(array($cust_email));
+        $total = $statement->rowCount();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        foreach($result as $row) {
+            $cust_status = $row['cust_status'];
+            $row_password = $row['cust_password'];
+        }
+
+        if($total==0) {
+            $error_message .= LANG_VALUE_133.'<br>';
+        } else {
+
+            if( $row_password != md5($cust_password) ) {
+                $error_message .= LANG_VALUE_139.'<br>';
+            } else {
+                if($cust_status == 0) {
+                    $error_message .= LANG_VALUE_148.'<br>';
+                } else {
+                    $_SESSION['customer'] = $row;
+                    header("location: ".BASE_URL."dashboard.php");
+                }
+            }
+            
+        }
+    }
+}
+?>
+
+    <div class="main-content">
+        <div class="wrap-banner">
+            <?php include("menu-category.php"); ?>
+            <nav class="breadcrumb-bg">
+                <div class="container no-index">
+                    <div class="breadcrumb">
+                        <ol>
+                            <li>
+                                <a href="#">
+                                    <span>Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Login</span>
+                                </a>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <!-- main -->
+        <div id="wrapper-site">
+            <div id="content-wrapper" class="full-width">
+                <div id="main">
+                    <div class="container">
+                        <h1 class="text-center title-page">Log In</h1>
+                        <div class="login-form">
+                           <form action="" method="post">
+                        <?php $csrf->echoInputField(); ?> 
+                        <?php
+                                if($error_message != '') {
+                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                                }
+                                if($success_message != '') {
+                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                                }
+                                ?>
+                                <div>
+                                    <input type="hidden" name="back" value="my-account">
+                                    <div class="form-group no-gutters">
+                                        <label for=""><?php echo LANG_VALUE_94; ?> *</label>
+                                           <input type="email" class="form-control" name="cust_email">
+                                    </div>
+                                    <div class="form-group no-gutters">
+                                        <label for=""><?php echo LANG_VALUE_96; ?> *</label><br>
+                                           <input type="password" class="form-control" name="cust_password">
+                                    </div>
+                                    <div class="no-gutters text-center">
+                                        <div class="forgot-password">
+                                            <a href="forget-password.php" style="color:#e4144d;"><?php echo LANG_VALUE_97; ?></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="clearfix">
+                                    <div class="text-center no-gutters">
+                                        <input type="hidden" name="submitLogin" value="1">
+                                         <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_4; ?>" name="form1">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once('footer.php'); ?>
