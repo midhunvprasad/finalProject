c9e33c2f8a2788928a2cecfc2a82b0460d875403
Arun Kumar <65325460+arunvudu@users.noreply.github.com>
2021-06-06 15:39:10 -0400

    Create cart-item-delete.php

---
+++ cart-item-delete.php
@@ -0,0 +1,63 @@
+<?php require_once('header.php'); ?>
+
+<?php
+
+// Check if the product is valid or not
+if( !isset($_REQUEST['id']) || !isset($_REQUEST['size']) || !isset($_REQUEST['color'])  ) {
+    header('location: cart.php');
+    exit;
+}
+
+$i=0;
+foreach($_SESSION['cart_p_id'] as $key => $value) {
+    $i++;
+    $arr_cart_p_id[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_size_id'] as $key => $value) {
+    $i++;
+    $arr_cart_size_id[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_size_name'] as $key => $value) {
+    $i++;
+    $arr_cart_size_name[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_color_id'] as $key => $value) {
+    $i++;
+    $arr_cart_color_id[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_color_name'] as $key => $value) {
+    $i++;
+    $arr_cart_color_name[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_p_qty'] as $key => $value) {
+    $i++;
+    $arr_cart_p_qty[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_p_current_price'] as $key => $value) {
+    $i++;
+    $arr_cart_p_current_price[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_p_name'] as $key => $value) {
+    $i++;
+    $arr_cart_p_name[$i] = $value;
+}
+
+$i=0;
+foreach($_SESSION['cart_p_featured_photo'] as $key => $value) {
+    $i++;
+    $arr_cart_p_featured_photo[$i] = $value;
+}
