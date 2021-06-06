1147690b4ba482738fde5bf42f8274ddac939ed2
Arun Kumar <65325460+arunvudu@users.noreply.github.com>
2021-06-06 15:44:22 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,213 @@
+<?php require_once('header.php'); ?>
+
+<body id="home2">
+    <?php require_once('main-header.php'); ?>
+
+    <!-- main content -->
+    <div class="main-content">
+        <div id="wrapper-site">
+           <div class="wrap-banner">
+            <?php include("menu-category.php"); ?>
+       
+                <!-- breadcrumb -->
+                <nav class="breadcrumb-bg">
+                    <div class="container no-index">
+                        <div class="breadcrumb">
+                            <ol>
+                                <li>
+                                    <a href="#">
+                                        <span>Home</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span>Contact</span>
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </nav>
+                <div id="main">
+                    <div class="page-home">
+                        <div class="container"><br><br>
+                            <h1 class="text-center title-page">Contact Us</h1>
+                            <div class="row-inhert">
+                                <div class="input-contact">
+                                    <p class="text-intro text-center">“Proin gravida nibh vel velit auctor aliquet. Aenean sollicudin, lorem quis bibendum
+                                        auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit
+                                        amet nibh vultate cursus a sit amet mauris. Proin gravida nibh vel velit auctor aliquet.”
+                                    </p>
+                                    
+                                    <p class="icon text-center">
+                                        <a href="#">
+                                            <img src="assets/img/other/contact_mess.png" alt="img">
+                                        </a>
+                                    </p>
+
+                                    <div class="d-flex justify-content-center">
+                                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                            <div class="contact-form">
+                                                                            <?php
+// After form submit checking everything for email sending
+if(isset($_POST['form_contact']))
+{
+    $error_message = '';
+    $success_message = '';
+    $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);                           
+    foreach ($result as $row) 
+    {
+        $receive_email = $row['receive_email'];
+        $receive_email_subject = $row['receive_email_subject'];
+        $receive_email_thank_you_message = $row['receive_email_thank_you_message'];
+    }
+
+    $valid = 1;
+
+    if(empty($_POST['visitor_name']))
+    {
+        $valid = 0;
+        $error_message .= 'Please enter your name.\n';
+    }
+
+    if(empty($_POST['visitor_phone']))
+    {
+        $valid = 0;
+        $error_message .= 'Please enter your phone number.\n';
+    }
+
+
+    if(empty($_POST['visitor_email']))
+    {
+        $valid = 0;
+        $error_message .= 'Please enter your email address.\n';
+    }
+    else
+    {
+        // Email validation check
+        if(!filter_var($_POST['visitor_email'], FILTER_VALIDATE_EMAIL))
+        {
+            $valid = 0;
+            $error_message .= 'Please enter a valid email address.\n';
+        }
+    }
+
+    if(empty($_POST['visitor_message']))
+    {
+        $valid = 0;
+        $error_message .= 'Please enter your message.\n';
+    }
+
+    if($valid == 1)
+    {
+        
+        $visitor_name = strip_tags($_POST['visitor_name']);
+        $visitor_email = strip_tags($_POST['visitor_email']);
+        $visitor_phone = strip_tags($_POST['visitor_phone']);
+        $visitor_message = strip_tags($_POST['visitor_message']);
+
+        // sending email
+        $to_admin = $receive_email;
+        $subject = $receive_email_subject;
+        $message = '
+<html><body>
+<table>
+<tr>
+<td>Name</td>
+<td>'.$visitor_name.'</td>
+</tr>
+<tr>
+<td>Email</td>
+<td>'.$visitor_email.'</td>
+</tr>
+<tr>
+<td>Phone</td>
+<td>'.$visitor_phone.'</td>
+</tr>
+<tr>
+<td>Comment</td>
+<td>'.nl2br($visitor_message).'</td>
+</tr>
+</table>
+</body></html>
+';
+       
+        
+        try {
+            
+        $mail->setFrom($visitor_email, $visitor_name);
+        $mail->addAddress($to_admin);
+        $mail->addReplyTo($visitor_email, $visitor_name);
+        
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+
+        $mail->Body = $message;
+        $mail->send();
+
+        $success_message = $receive_email_thank_you_message;
+    } catch (Exception $e) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
+        
+        
+
+    }
+}
+?>
+                
+                <?php
+                if($error_message != '') {
+                    echo "<script>alert('".$error_message."')</script>";
+                }
+                if($success_message != '') {
+                    echo "<script>alert('".$success_message."')</script>";
+                }
+                ?>
+                                                 <form action="" method="post">
+                                                   <?php $csrf->echoInputField(); ?>
+                                                    <div class="form-fields">
+                                                        <div class="form-group row">
+                                                            <div class="col-md-6">
+                                                                <input class="form-control" name="visitor_name" placeholder="Your name">
+                                                            </div>
+                                                            <div class="col-md-6 margin-bottom-mobie">
+                                                                <input class="form-control" name="visitor_email"  type="email" value="" placeholder="Your email">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <div class="col-md-12 margin-bottom-mobie">
+                                                                <input class="form-control" name="visitor_phone"   type="number" value="" placeholder="Your phone number">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group row">
+                                                            <div class="col-md-12">
+                                                                <textarea class="form-control" name="visitor_message" placeholder="Message" rows="8"></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <button class="btn" type="submit" value="Send Message" name="form_contact">
+                                                            <img class="img-fl" src="assets/img/other/contact_email.png" alt="img">Send message
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php require_once('footer.php'); ?>
+    
