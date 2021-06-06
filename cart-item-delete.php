97dc833bb794620bbd8d8008667ddc10ad939188
Arun Kumar <65325460+arunvudu@users.noreply.github.com>
2021-06-06 15:39:48 -0400

    Update cart-item-delete.php

--- cart-item-delete.php
+++ cart-item-delete.php
@@ -61,3 +61,12 @@ foreach($_SESSION['cart_p_featured_photo'] as $key => $value) {
     $i++;
     $arr_cart_p_featured_photo[$i] = $value;
 }
+unset($_SESSION['cart_p_id']);
+unset($_SESSION['cart_size_id']);
+unset($_SESSION['cart_size_name']);
+unset($_SESSION['cart_color_id']);
+unset($_SESSION['cart_color_name']);
+unset($_SESSION['cart_p_qty']);
+unset($_SESSION['cart_p_current_price']);
+unset($_SESSION['cart_p_name']);
+unset($_SESSION['cart_p_featured_photo']);
